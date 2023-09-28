e82d567441f44c2d88402aeac268d5c293506ad2
Dialejo24 <dialejosi530@gmail.com>
2023-09-27 22:17:43 -0500

    Create header and append it to content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("probando");
+let content = document.getElementById("content");
+let header = document.createElement("header");
+let nav = document.createElement("nav");
+let ul = document.createElement("ul");
+
+ul.appendChild(createListItem("Home", "home"));
+ul.appendChild(createListItem("Menu", "menu"));
+ul.appendChild(createListItem("Contact", "contact"));
+
+nav.appendChild(ul);
+header.appendChild(nav);
+content.appendChild(header);
+
+function createListItem(text, id) {
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+    link.textContent = text;
+    li.appendChild(link);
+    li.setAttribute("id", id);
+    return li;
+}
