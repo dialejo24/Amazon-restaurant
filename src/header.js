e3191a8bf1ee737693cd959a3aa350b36ce50c23
Dialejo24 <dialejosi530@gmail.com>
2023-09-27 22:45:37 -0500

    change index.js name to header.js and create a new index.js file

---
+++ src/header.js
@@ -0,0 +1,24 @@
+import './style.css';
+
+let content = document.getElementById("content");
+let header = document.createElement("header");
+let nav = document.createElement("nav");
+let ul = document.createElement("ul");
+
+ul.appendChild(createListItem("Home", "home"));
+ul.appendChild(createListItem("Menu", "menu"));
+ul.appendChild(createListItem("Contact", "contact"));
+
+nav.appendChild(ul);
+header.appendChild(nav);
+content.appendChild(header);
+
+function createListItem(text, id) {
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+    link.textContent = text;
+    link.setAttribute("href", "#");
+    li.appendChild(link);
+    li.setAttribute("id", id);
+    return li;
+}
